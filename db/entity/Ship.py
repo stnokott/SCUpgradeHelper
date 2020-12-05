828bee4854fc8b7aa633d84d49a793b8c42127b2
Noah Kottenhahn <noah.kottenhahn@stud.h-da.de>
2020-12-05 20:41:02 +0100

    init repo

---
+++ db/entity/Ship.py
@@ -0,0 +1,20 @@
+"""File for Ship class"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, Float, String
+
+Base = declarative_base()
+
+
+class Ship(Base):
+    """
+    Class representing a purchasable ship or vehicle
+    """
+    __tablename__ = 'SHIPS'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    manufacturer = Column(String)
+    price_dollar = Column(Float)
+
+    def __repr__(self):
+        return "<%s@%d(%s, $%d)>" % self.__name__, self.id, self.name, self.price_dollar
