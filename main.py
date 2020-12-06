ad78cbe03be49ccc5b8b5b295828d95b029c2473
Noah Kottenhahn <noah.kottenhahn@stud.h-da.de>
2020-12-06 16:13:24 +0100

    create main

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from db.util.entitymanager import EntityManager
+"""main"""
+from api import SCApi
+from config import ConfigProvider
+from db.util import EntityManager
 
 if __name__ == '__main__':
     em = EntityManager()
+    config = ConfigProvider()
+    api = SCApi(config.sc_api_key)
+    print(api.get_ships())
