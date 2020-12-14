db892560abfd1ad37fc97cfd4475b98ea7477ec7
Noah Kottenhahn <noah.kottenhahn@stud.h-da.de>
2020-12-14 12:19:52 +0100

    remove redundant logging filter

--- const.py
+++ const.py
@@ -1,17 +1,8 @@
 """Contains constants used application-wide"""
 from datetime import timedelta
-from logging import Filter
 
 DATABASE_FILEPATH = "database.db"
 CONFIG_FILEPATH = "config.ini"
 
 SHIP_DATA_EXPIRY = timedelta(days=7)
 UPGRADE_DATA_EXPIRY = timedelta(days=1)
-
-
-class __SuppressAllLoggingFilter(Filter):
-    def filter(self, record) -> bool:
-        return False
-
-
-SUPPRESS_ALL_LOGGING_FILTER = __SuppressAllLoggingFilter()
