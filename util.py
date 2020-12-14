9d838633af0bbc8e75d3fff01339f31fea6547ac
Noah Kottenhahn <noah.kottenhahn@stud.h-da.de>
2020-12-14 12:55:55 +0100

    add and use timedelta formatter

--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 """Provides utility classes and functions"""
+from datetime import timedelta
 
 
 class StatusString:
@@ -26,3 +27,25 @@ class StatusString:
             return f"### {self.__DONE_STR} ###"
         surrounding_hashs = round((len(self.__status) - len(self.__DONE_STR)) / 2) * "#"
         return f"###{surrounding_hashs} {self.__DONE_STR} {surrounding_hashs}###"
+
+
+def format_timedelta(delta: timedelta) -> str:
+    """
+    Create nicely formatted string from timedelta
+    Args:
+        delta: timedelta to process
+
+    Returns:
+        formatted string from timedelta
+    """
+    output_str = ""
+    total_seconds = delta.total_seconds()
+    if delta.days > 0:
+        output_str += f"{delta.days} days, "
+    total_seconds -= delta.days * 86400
+    hours = str(round(total_seconds // 3600)).zfill(2)
+    minutes = str(round((total_seconds % 3600) // 60)).zfill(2)
+    seconds = str(round(total_seconds % 60)).zfill(2)
+
+    output_str += f"{hours}:{minutes}:{seconds}"
+    return output_str
