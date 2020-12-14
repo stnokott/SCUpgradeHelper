71bbb99a63ea1c171ea4e37005de9444581bcc4c
Noah Kottenhahn <noah.kottenhahn@stud.h-da.de>
2020-12-14 12:56:35 +0100

    modify main

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ from config import ConfigProvider
 
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
     logger.addHandler(logging.StreamHandler())
 
     config = ConfigProvider(logger)
     broker = SCDataBroker(logger, config)
-    print(broker.get_ships())
