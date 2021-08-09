cd31acf07ddee8051694c783d749ec44a4e60462
stnokott <nftrash@web.de>
2021-08-09 14:55:13 +0200

    add TODOs

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-"""main"""
+"""test"""
 import logging
 
 from broker import SCDataBroker
@@ -6,9 +6,16 @@ from config import ConfigProvider
 from util import CustomLogger
 
 if __name__ == "__main__":
-    logger = CustomLogger(__name__, logging.DEBUG)
+    # TODO: add support for adding already existing own upgrades
+    # TODO: use Decimal instead of float where possible
+    logger = CustomLogger(__name__, logging.INFO)
 
     config = ConfigProvider(logger)
     broker = SCDataBroker(logger, config)
-    # broker.complete_update(echo=True)
-    # print(broker.get_ships())
+    broker.complete_update(False, True)
+    path = broker.get_purchase_path(22)
+    if path is not None:
+        # path.full_print(logger)
+        print(path)
+    else:
+        logger.info("No upgrade path found.")
