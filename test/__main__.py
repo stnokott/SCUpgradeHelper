3c682ecada48fa62d0c84c8b28539d714465c3a4
stnokott <nftrash@web.de>
2021-08-09 15:35:24 +0200

    use command line args to build config file

--- test/__main__.py
+++ test/__main__.py
@@ -1,4 +1,27 @@
+import os
+import sys
+
 import pytest
 
+from const import CONFIG_FILEPATH
+
 if __name__ == "__main__":
+    config_file_dir = os.path.abspath(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
+    )
+    config_file_path = os.path.join(config_file_dir, CONFIG_FILEPATH)
+    if not os.path.exists(config_file_path):
+        sc_api_key = sys.argv[0]
+        reddit_client_id = sys.argv[1]
+        reddit_client_secret = sys.argv[2]
+        with open(config_file_path, "w") as config_file:
+            config_file.write(
+                f"""
+            [AUTH]
+            scapikey={sc_api_key}
+            redditclientid={reddit_client_id}
+            redditclientsecret={reddit_client_secret}
+            """
+            )
+
     retcode = pytest.main()
